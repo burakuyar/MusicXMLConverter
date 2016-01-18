1b77ce3d4deca06410807b4671a4028dd2617f27
burakuyar <uyarburak@msn.com>
2016-01-19 01:58:57 +0200

    setup.py MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='musicxmlconverter',
+      version='0.1',
+      author='Burak Uyar',
+      author_email='burakuyar@gmail.com',
+      license='agpl 3.0',
+      description='Tool for generating a MusicXML file from a SymbTr txt file.',
+      url='https://github.com/burakuyar',
+      packages=['musicxmlconverter'],
+      include_package_data=True,
+)
