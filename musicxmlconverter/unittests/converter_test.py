1fed4ae33fa623f9d39d140c460e1f55ecce44ff
Sertan Senturk <contact@sertansenturk.com>
2016-04-11 17:39:00 +0200

    Move unittests inside the package

--- musicxmlconverter/unittests/converter_test.py
+++ musicxmlconverter/unittests/converter_test.py
@@ -10,10 +10,9 @@ def test_converter():
     mu2path = os.path.join('sampledata', scorename + '.mu2')
     xmlpath = os.path.join('sampledata', scorename + '.xml')
 
-    mbid_url = 'http://musicbrainz.org/work/50bc4b54-5e14-4a98-bd44-ee5493479c7d'
+    url = 'http://musicbrainz.org/work/50bc4b54-5e14-4a98-bd44-ee5493479c7d'
 
-    piece = SymbTrScore(txtpath, mu2path, symbtrname=scorename,
-                        mbid_url=mbid_url)
+    piece = SymbTrScore(txtpath, mu2path, symbtrname=scorename, mbid_url=url)
     xmlstr = piece.convertsymbtr2xml()  # outputs the xml score as string
 
     with open(xmlpath, 'r') as savedxmlfile:
