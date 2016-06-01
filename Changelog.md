8ff84e4b084cd59bbe57b933a8ed74ddf4d8a9bb
Sertan Senturk <contact@sertansenturk.com>
2016-06-02 01:01:11 +0300

    Updated the requirements

--- Changelog.md
+++ Changelog.md
@@ -1,13 +1,16 @@
-#### MusicXmlConverter v1.2.0
+#### MusicXMLConverter v1.2.1
+ - Updated the requirement packages
+
+#### MusicXMLConverter v1.2.0
  - Refactoring according tp PEP8 conventions
  - Added missing makam and usul attributes
  - Froze requirements
 
-#### MusicXmlConverter v1.1
+#### MusicXMLConverter v1.1
  - Refactored the code to call the new symbtrdataextractor v2.0.0-alpha syntax
 
-#### MusicXmlConverter v1.0.1
+#### MusicXMLConverter v1.0.1
  - Changed MBID dictionary input to MusicBrainz url for simplicity
  
-#### MusicXmlConverter v1.0
+#### MusicXMLConverter v1.0
  - First public release
--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ from setuptools import setup
 
 
 setup(name='musicxmlconverter',
-      version='1.2.0',
+      version='1.2.1',
       author='Burak Uyar',
       author_email='burakuyar@gmail.com',
       license='agpl 3.0',
-      description='Tool for generating a MusicXML file from a SymbTr txt file.',
+      description='Tool for generating a MusicXML file from a SymbTr txt '
+                  'file.',
       url='https://github.com/burakuyar',
       packages=find_packages(),
       install_requires=[
-          'numpy<=1.11.0',
-          'lxml==3.6.0'
+          'numpy>=1.9.0',
+          'lxml>=3.6.0'
       ],
       include_package_data=True,
       )
